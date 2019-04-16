f6cc767c9f76ec72ae8644332b0245f300cb1362
tranhuyphuhp75 <tranhuyphuhp75@hmail.com>
2019-04-16 23:50:33 +0700

    add function login

---
+++ DatingApp.API/Data/IAuthRepository.cs
@@ -0,0 +1,11 @@
+using System.Threading.Tasks;
+using DatingApp.API.Model;
+namespace DatingApp.API.Data
+{
+    public interface IAuthRepository
+    {
+         Task<User> Register(User user , string password);
+         Task<User> Login(string username, string password);
+         Task<bool> UserExist(string username); 
+    }
+}
